b3e5e9bfea63446af3192fca3883b24835319cc3
MaratKhambelov <maratkhambelov@gmail.com>
2024-03-19 17:41:48 +0300

    Создать прототип двух устройств ""умного дома"": умной розетки и термометра

---
+++ src/devices.rs
@@ -0,0 +1,21 @@
+trait SmartPlug  {
+    fn get_info(){
+    todo!()
+    }
+    fn get_power_consumption() -> f64{
+        todo!()
+    }
+    fn on(){
+    todo!()
+    }
+    fn off(){
+    todo!()
+    }
+}
+
+
+trait Thermometer  {
+    fn get_temperature() -> f32{
+        todo!()
+    }
+}
